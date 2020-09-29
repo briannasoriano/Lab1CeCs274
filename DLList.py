46fc8ccc0a49a0dd5ebaad050e580658d0e5edcc
Brianna <soriano.brianna01@gmail.com>
2020-09-29 00:58:21 -0700

    first commit

---
+++ DLList.py
@@ -0,0 +1,79 @@
+from Interfaces import List
+import numpy as np
+
+
+class DLList(List):
+    class Node:
+        def __init__(self, x : np.object) :
+            self.next = None
+            self.prev = None
+            self.x = x
+
+    def __init__(self) :
+        self.dummy = DLList.Node("")
+        self.dummy.next = self.dummy
+        self.dummy.prev = self.dummy
+        self.n = 0
+   
+    def get_node(self, i : int) -> Node:
+        # todo
+        pass 
+        
+    def get(self, i) -> np.object:
+        # todo
+        pass 
+
+    def set(self, i : int, x : np.object) -> np.object:
+        # todo
+        pass 
+
+    def add_before(self, w : Node, x : np.object) -> Node:
+        # todo
+        pass 
+            
+    def add(self, i : int, x : np.object)  :
+        # todo
+        pass 
+
+    def _remove(self, w : Node) :
+       # todo
+        pass 
+    
+    def remove(self, i :int) :
+        if i < 0 or i > self.n:  raise IndexError()
+        return self._remove(self.get_node(i))
+
+
+    def size(self) -> int:
+        return self.n
+
+    def append(self, x : np.object)  :
+        self.add(self.n, x)
+
+    def isPalindrome(self) -> bool :
+        # todo
+        pass 
+
+
+    def __str__(self):
+        s = "["
+        u = self.dummy.next
+        while u is not self.dummy:
+            s += "%r" % u.x
+            u = u.next
+            if u is not None:
+                s += ","
+        return s + "]"
+
+
+    def __iter__(self):
+        self.iterator = self.dummy.next
+        return self
+
+    def __next__(self):
+        if self.iterator != self.dummy:
+            x = self.iterator.x
+            self.iterator = self.iterator.next
+        else:
+             raise StopIteration()
+        return x
